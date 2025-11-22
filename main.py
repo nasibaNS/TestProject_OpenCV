0d7354d793d7df6a585a0f789830175102196f1c
nasibaNS <joldoshbekovanasiba31.com>
2025-11-22 12:31:20 +0600

    Загрузила модель. Создала папку для хр-я видео. Открыла доступ к камере и задала окно для отображения видео с размером

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import streamlit as st
 import time
 import numpy as np
 import tempfile
+
+
+model = YOLO("yolov8n.pt")
+
+video_path = 'videos'
+os.makedirs(video_path, exist_ok=True)
+
+cap = cv2.VideoCapture(0)
+
+
